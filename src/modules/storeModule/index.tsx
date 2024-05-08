d3a24376e9da9ad0bd6709083aecf15d04a6b24b
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-05-08 11:22:56 +0530

    store route created and demo route removed

---
+++ src/modules/storeModule/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./storemodule.module.scss";
+
+const DashboardModule = () => {
+  return <div className={styles.dashboardModuleWrapper}>DashboardModule</div>;
+};
+
+export default DashboardModule;
--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,19 +1,19 @@
-import MainLayout from "@/layout/MainLayout";
-import DemoModule from "@/modules/demoModule";
 import { NextPage } from "next";
 import Head from "next/head";
+import MainLayout from "@/layout/MainLayout";
+import StoreModule from "@/modules/storeModule";
 
-const DemoPage: NextPage = () => {
+const Store: NextPage = () => {
   return (
     <>
       <Head>
         <title>OMS Panel</title>
       </Head>
       <MainLayout>
-        <DemoModule />
+        <StoreModule />
       </MainLayout>
     </>
   );
 };
 
-export default DemoPage;
+export default Store;
