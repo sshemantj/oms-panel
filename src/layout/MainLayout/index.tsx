466c0ff6a70b1dbf478661a54e3d27eee062cd61
Hemant Jadhav (Innova Consultant) <CON10201@shoppersstop.com>
2024-05-16 13:28:27 +0530

    changes

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -1,22 +1,11 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-// import HamIcon from "@/component/atoms/hamIcon";
-import SearchIcon from "@mui/icons-material/Search";
-import { useMobileCheck } from "@/hooks/useMobileCheck";
-// import SearchComponent from "@/component/atoms/searchComponent";
+import React, { useState } from "react";
 import RhsWrapper from "./RhsWrapper";
 import NavList from "./navlist";
-// import Breadcrumbs from "@/component/atoms/breadcrumb";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { IProducts } from "@/store/slices/dashboardSlice";
 import { useRouter } from "next/router";
 import { IAllRoutes, IListRoutes } from "@/constants/allRoutes";
 import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
-import { useSearchParams } from "next/navigation";
-import { Cookies } from "react-cookie";
 import HeaderLabel from "@/component/atoms/headerLabel";
 import styles from "./newNavbar.module.scss";
-
-const cookie = new Cookies();
 
 interface IProps {
   children: JSX.Element;
@@ -25,117 +14,17 @@ interface IProps {
 
 const MainLayout = (props: IProps) => {
   const { children, mainStyle = {} } = props;
-  const isMobile = useMobileCheck();
-  const inputRef = useRef<any>(null);
-  const dispatch = useAppDispatch();
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const screen = searchParams.get("screen");
-  const [isSearchActive, setIsSearchActive] = useState<boolean>(
-    window.innerWidth < 768
-  );
+
   const [isNavOpen, setisNavOpen] = useState<boolean>(false);
-  const [openSelect, setOpenSelect] = useState<boolean>(false);
-  const [currValue, setCurrValue] = useState("");
-  const [productType, setProductType] = useState<IProducts>();
-
-  const { userChannelMappings, selectedChannel, pdType } = useAppSelector(
-    (state) => state.dashboard
-  );
-
-  const isDashboard = useMemo(
-    () => router.pathname === IAllRoutes.DASHBOARD,
-    [router.pathname]
-  );
-
-  const channelMappingsArr =
-    (Array.isArray(userChannelMappings) &&
-      userChannelMappings?.map((item: any) => {
-        return {
-          label: item.channelName,
-          value: item.channelId,
-        };
-      })) ||
-    [];
-
-  useEffect(() => {
-    if (isDashboard) {
-      // dispatch(getUserChannelMappings());
-      screen && setProductType(screen as IProducts);
-    }
-  }, [router, screen]);
-
-  // const isUnapprovedScreen = screen === IProductsTypes.UNAPPROVED;
-
-  // useEffect(() => {
-  //   if (screen && !isUnapprovedScreen) {
-  //     if (cookie.get("token") && inputRef && !currValue && !selectedChannel) {
-  //       openChannelDropdown();
-  //     }
-  //   } else if (isUnapprovedScreen) {
-  //     getTableData({ type: IProductsTypes.UNAPPROVED });
-  //     dispatch(changePdType(IProductsTypes.UNAPPROVED));
-  //   }
-  // }, [inputRef, currValue, isUnapprovedScreen, screen]);
-
-  useEffect(() => {
-    setCurrValue(selectedChannel);
-  }, [selectedChannel]);
-
-  const handleSelectChange = async (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { value } = e.target;
-    // dispatch(setChannelMapping(value));
-    setCurrValue(value);
-    setOpenSelect(false);
-    // dispatch(resetHomeTableData());
-    // productType && dispatch(changePdType(productType as IProducts));
-  };
 
   const handleHamClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
     setisNavOpen((prev) => !prev);
   };
 
-  const openChannelDropdown = () => {
-    inputRef && inputRef?.current?.focus?.();
-    setOpenSelect(true);
-  };
-
-  const handleProductState = (value: any, path?: any) => {
-    // dispatch(resetHomeTableData());
-    // dispatch(changePdType(value));
-    const url = `${IAllRoutes.DASHBOARD}?screen=${value}`;
-    if (!isDashboard) {
-      router.push(url);
-    } else {
-      router.push(url);
-      // if (value === IProductsTypes.UNAPPROVED) {
-      //   getTableData({});
-      //   return;
-      // }
-      if (!selectedChannel) {
-        openChannelDropdown();
-      }
-    }
-  };
-
   const handleTypeClick = (value: any, path: IAllRoutes | IListRoutes) => {
-    setProductType(value);
-    // switch (value) {
-    //   case "map_user_with_channels":
-    //     router.push("/map-user-channels");
-    //     return;
-    // }
-
     switch (path) {
-      // case IAllRoutes.DASHBOARD:
-      //   handleProductState(value);
-      //   return;
-      // case IAllRoutes.MANAGE_CHANNELS:
-      // case IAllRoutes.USER_PROFILE:
-      // case IAllRoutes.FILE_MANAGEMENT:
       case IAllRoutes.CARRIER_COLLECTIONS:
       case IAllRoutes.FULFILLMENTS:
         router.push(`${path}`);
