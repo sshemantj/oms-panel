d1c7130f75f107952e8138c32f6d35e1e60d4c6c
Muralidevan <murali@theretailinsights.com>
2024-08-02 12:09:05 +0530

    test: login api

--- src/pages/api/auth/authenticate.ts
+++ src/pages/api/auth/authenticate.ts
@@ -30,67 +30,70 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       console.log(authenticateResponse, "authenticateResponse authenticte");
       console.log("authenticateResponse.status", authenticateResponse.status);
 
-      if (
-        authenticateResponse.status === 200 &&
-        (authenticateResponse.data.accessToken ||
-          authenticateResponse.data ===
-            "You must change your password before continuing.")
-      ) {
-        // Get user details
-        const config = {
-          method: "get",
-          maxBodyLength: Infinity,
-          url: `/users/GetByUsername?username=${username}`,
-          headers: {},
-        };
+      // if (
+      //   authenticateResponse.status === 200 &&
+      //   (authenticateResponse.data.accessToken ||
+      //     authenticateResponse.data ===
+      //       "You must change your password before continuing.")
+      // ) {
+      //   // Get user details
+      //   const config = {
+      //     method: "get",
+      //     maxBodyLength: Infinity,
+      //     url: `/users/GetByUsername?username=${username}`,
+      //     headers: {},
+      //   };
 
-        try {
-          const userResponse: any = await axiosPublic(config);
-          console.log("userResponse cehck", userResponse);
-          if (
-            userResponse.status === 200 &&
-            userResponse.data &&
-            userResponse.data.userName
-          ) {
-            req.session.user = {
-              accessToken: authenticateResponse.data.accessToken,
-              id: userResponse.data.id || "",
-              userName: userResponse.data.userName || "",
-              email: userResponse.data.email || "",
-              role: userResponse.data.role || "",
-              storecode: userResponse.data.storecode || "",
-              changePwdAtLogon: userResponse.data.changePwdAtLogon || false,
-            };
-            console.log(" req.session", req.session);
+      //   try {
+      //     const userResponse: any = await axiosPublic(config);
+      //     console.log("userResponse cehck", userResponse);
+      //     if (
+      //       userResponse.status === 200 &&
+      //       userResponse.data &&
+      //       userResponse.data.userName
+      //     ) {
+      //       req.session.user = {
+      //         accessToken: authenticateResponse.data.accessToken,
+      //         id: userResponse.data.id || "",
+      //         userName: userResponse.data.userName || "",
+      //         email: userResponse.data.email || "",
+      //         role: userResponse.data.role || "",
+      //         storecode: userResponse.data.storecode || "",
+      //         changePwdAtLogon: userResponse.data.changePwdAtLogon || false,
+      //       };
+      //       console.log(" req.session", req.session);
 
-            await req.session.save();
+      //       await req.session.save();
 
-            const isFirstTime =
-              authenticateResponse.data ===
-              "You must change your password before continuing.";
+      //       const isFirstTime =
+      //         authenticateResponse.data ===
+      //         "You must change your password before continuing.";
 
-            return res.status(200).json({
-              message: isFirstTime
-                ? authenticateResponse.data
-                : "Login Successfull",
-              success: true,
-              role: userResponse.data.role,
-            });
-          } else {
-            return res.status(400).json({
-              success: false,
-            });
-          }
-        } catch (error: any) {
-          return res
-            .status(500)
-            .json({ message: error.message, success: false });
-        }
-      } else {
-        return res
-          .status(200)
-          .json({ message: authenticateResponse.data, success: false });
-      }
+      //       return res.status(200).json({
+      //         message: isFirstTime
+      //           ? authenticateResponse.data
+      //           : "Login Successfull",
+      //         success: true,
+      //         role: userResponse.data.role,
+      //       });
+      //     } else {
+      //       return res.status(400).json({
+      //         success: false,
+      //       });
+      //     }
+      //   } catch (error: any) {
+      //     return res
+      //       .status(500)
+      //       .json({ message: error.message, success: false });
+      //   }
+      // } else {
+      //   return res
+      //     .status(200)
+      //     .json({ message: authenticateResponse.data, success: false });
+      // }
+      return res
+        .status(200)
+        .json({ message: authenticateResponse.data.message, success: false });
     } catch (error: any) {
       console.log("error", error);
       return res.status(500).json({ message: error.message, success: false });
