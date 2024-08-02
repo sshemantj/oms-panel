ec58f678efde853d6a12080bdb4d5ea914a16853
Muralidevan <murali@theretailinsights.com>
2024-08-02 12:57:02 +0530

    fix: login test done

--- src/pages/api/auth/authenticate.ts
+++ src/pages/api/auth/authenticate.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withSession from "@/lib/session";
 import { axiosPublic } from "@/services/client";
-import axios from "axios";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("req", req);
@@ -17,85 +16,81 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       let config = {
         method: "post",
         maxBodyLength: Infinity,
-        url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/authenticate`,
+        url: "/users/authenticate",
         headers: {
           "Content-Type": "application/json",
         },
         data: data,
-        timeout: 30000,
       };
 
       console.log("config", config);
 
-      const authenticateResponse: any = await axios.request(config);
+      const authenticateResponse: any = await axiosPublic(config);
 
       console.log(authenticateResponse, "authenticateResponse authenticte");
       console.log("authenticateResponse.status", authenticateResponse.status);
 
-      // if (
-      //   authenticateResponse.status === 200 &&
-      //   (authenticateResponse.data.accessToken ||
-      //     authenticateResponse.data ===
-      //       "You must change your password before continuing.")
-      // ) {
-      //   // Get user details
-      //   const config = {
-      //     method: "get",
-      //     maxBodyLength: Infinity,
-      //     url: `/users/GetByUsername?username=${username}`,
-      //     headers: {},
-      //   };
+      if (
+        authenticateResponse.status === 200 &&
+        (authenticateResponse.data.accessToken ||
+          authenticateResponse.data ===
+            "You must change your password before continuing.")
+      ) {
+        // Get user details
+        const config = {
+          method: "get",
+          maxBodyLength: Infinity,
+          url: `/users/GetByUsername?username=${username}`,
+          headers: {},
+        };
 
-      //   try {
-      //     const userResponse: any = await axiosPublic(config);
-      //     console.log("userResponse cehck", userResponse);
-      //     if (
-      //       userResponse.status === 200 &&
-      //       userResponse.data &&
-      //       userResponse.data.userName
-      //     ) {
-      //       req.session.user = {
-      //         accessToken: authenticateResponse.data.accessToken,
-      //         id: userResponse.data.id || "",
-      //         userName: userResponse.data.userName || "",
-      //         email: userResponse.data.email || "",
-      //         role: userResponse.data.role || "",
-      //         storecode: userResponse.data.storecode || "",
-      //         changePwdAtLogon: userResponse.data.changePwdAtLogon || false,
-      //       };
-      //       console.log(" req.session", req.session);
+        try {
+          const userResponse: any = await axiosPublic(config);
+          console.log("userResponse cehck", userResponse);
+          if (
+            userResponse.status === 200 &&
+            userResponse.data &&
+            userResponse.data.userName
+          ) {
+            req.session.user = {
+              accessToken: authenticateResponse.data.accessToken,
+              id: userResponse.data.id || "",
+              userName: userResponse.data.userName || "",
+              email: userResponse.data.email || "",
+              role: userResponse.data.role || "",
+              storecode: userResponse.data.storecode || "",
+              changePwdAtLogon: userResponse.data.changePwdAtLogon || false,
+            };
+            console.log(" req.session", req.session);
 
-      //       await req.session.save();
+            await req.session.save();
 
-      //       const isFirstTime =
-      //         authenticateResponse.data ===
-      //         "You must change your password before continuing.";
+            const isFirstTime =
+              authenticateResponse.data ===
+              "You must change your password before continuing.";
 
-      //       return res.status(200).json({
-      //         message: isFirstTime
-      //           ? authenticateResponse.data
-      //           : "Login Successfull",
-      //         success: true,
-      //         role: userResponse.data.role,
-      //       });
-      //     } else {
-      //       return res.status(400).json({
-      //         success: false,
-      //       });
-      //     }
-      //   } catch (error: any) {
-      //     return res
-      //       .status(500)
-      //       .json({ message: error.message, success: false });
-      //   }
-      // } else {
-      //   return res
-      //     .status(200)
-      //     .json({ message: authenticateResponse.data, success: false });
-      // }
-      return res
-        .status(200)
-        .json({ message: authenticateResponse.data.message, success: false });
+            return res.status(200).json({
+              message: isFirstTime
+                ? authenticateResponse.data
+                : "Login Successfull",
+              success: true,
+              role: userResponse.data.role,
+            });
+          } else {
+            return res.status(400).json({
+              success: false,
+            });
+          }
+        } catch (error: any) {
+          return res
+            .status(500)
+            .json({ message: error.message, success: false });
+        }
+      } else {
+        return res
+          .status(200)
+          .json({ message: authenticateResponse.data, success: false });
+      }
     } catch (error: any) {
       console.log("error", error);
       return res.status(500).json({ message: error.message, success: false });
